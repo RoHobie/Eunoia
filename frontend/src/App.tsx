bbcdcfebacabb0005c6748857ea2f233a9f7c02e
RoHobie <ronitthakur267@gmail.com>
2025-08-15 20:22:30 +0530

    INITIAL COMMIT: frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold underline">
+        Hello Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
